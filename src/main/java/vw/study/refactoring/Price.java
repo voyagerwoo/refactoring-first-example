885299f9024dc60b02b673c5e29d7a313fc1bfa0
voyagerwoo <voyager.woo@gmail.com>
2018-11-19 01:36:16 +0900

    적립포인트 계산 메서드를 조건문에서 재정의로 전환

--- src/main/java/vw/study/refactoring/Price.java
+++ src/main/java/vw/study/refactoring/Price.java
@@ -3,4 +3,8 @@ package vw.study.refactoring;
 public abstract class Price {
     abstract int getPriceCode();
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
