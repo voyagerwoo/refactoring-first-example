74f0aaba54572da39cbedd682f94059f88b55879
voyagerwoo <voyager.woo@gmail.com>
2018-11-19 01:33:13 +0900

    가격 계산 메서드를 조건문에서 재정의로 전환

--- src/main/java/vw/study/refactoring/ChildrensPrice.java
+++ src/main/java/vw/study/refactoring/ChildrensPrice.java
@@ -5,4 +5,13 @@ public class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+
+        return result;
+    }
 }
--- src/main/java/vw/study/refactoring/RegularPrice.java
+++ src/main/java/vw/study/refactoring/RegularPrice.java
@@ -5,4 +5,12 @@ public class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
 }
