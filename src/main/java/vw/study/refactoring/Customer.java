3ea57d0e0073fea2cea14d1642e39049383b1e33
voyagerwoo <voyager.woo@gmail.com>
2018-11-18 23:22:38 +0900

    의미에 맞게 변수명 수정

--- src/main/java/vw/study/refactoring/Customer.java
+++ src/main/java/vw/study/refactoring/Customer.java
@@ -42,23 +42,23 @@ public class Customer {
     }
 
     private double amountFor(Rental each) {
-        double thisAmount = 0;
+        double result = 0;
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
+                result += 2;
                 if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    result += (each.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                result += each.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
+                result += 1.5;
                 if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    result += (each.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return result;
     }
 
     public String getName() {
