f52be174283719bb79b2305a37133cb7e71b39e7
voyagerwoo <voyager.woo@gmail.com>
2018-11-18 23:45:16 +0900

    적립포인트 총합 계산 메서드 추출

--- src/main/java/vw/study/refactoring/Customer.java
+++ src/main/java/vw/study/refactoring/Customer.java
@@ -12,24 +12,29 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
-        Enumeration rentals = _rentals.elements();
         String result = _name + " 고객님의 대여 기록\n";
+        Enumeration rentals = _rentals.elements();
 
         while(rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-
-            // 경우에 따른 적립 포인트 지급 함수를 호출
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
         }
 
         //푸터 행 추가
         result += "누적 대여료: " + String.valueOf(getTotalCharge()) + "\n";
-        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+        result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints());
         return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            frequentRenterPoints += each.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
     }
 
     private double getTotalCharge() {
