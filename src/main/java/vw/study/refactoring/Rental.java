9c1e9f7734a2ea43c7da0a394c8a326a3289b45c
voyagerwoo <voyager.woo@gmail.com>
2018-11-18 23:35:40 +0900

    적립 포인트 계산 메서드 추출 후 메서드 이동

--- src/main/java/vw/study/refactoring/Rental.java
+++ src/main/java/vw/study/refactoring/Rental.java
@@ -36,4 +36,10 @@ public class Rental {
         }
         return result;
     }
+
+    int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        return 1;
+    }
 }
