03ac78d0c83899ed8867e801e18a5da5d109736c
voyagerwoo <voyager.woo@gmail.com>
2018-11-19 00:09:25 +0900

    htmlStatement 메서드에 대한 테스트 추가

--- src/test/java/vw/study/refactoring/CustomerTest.java
+++ src/test/java/vw/study/refactoring/CustomerTest.java
@@ -81,4 +81,85 @@ public class CustomerTest {
                 "가디언즈 오브 갤럭시 1\t2.0\n\t가디언즈 오브 갤럭시 2\t6.5\n\t서치\t3.0\n\t상어가족\t4.5\n" +
                 "누적 대여료: 16.0\n적립 포인트: 4"));
     }
+
+    @Test
+    public void htmlStatement_no_rented() {
+        //given
+        Customer customer = new Customer("홍길동");
+        //when
+        String htmlStatement = customer.htmlStatement();
+
+        //then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement, is("<h1>홍길동 고객님의 대여 기록</h1><p>\n<p>누적 대여료: <em>0.0</em><p>\n<p>적립 포인트: <em>0</em><p>"));
+    }
+
+    @Test
+    public void htmlStatement_1() {
+        //given
+        Movie movie1 = new Movie("가디언즈 오브 갤럭시 1", Movie.REGULAR);
+        Movie movie4 = new Movie("상어가족", Movie.CHILDRENS);
+
+        Customer customer = new Customer("홍길동");
+        customer.addRental(new Rental(movie1, 1));
+        customer.addRental(new Rental(movie4, 1));
+        //when
+        String htmlStatement = customer.htmlStatement();
+
+        //then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement, is("<h1>홍길동 고객님의 대여 기록</h1><p>\n" +
+                "가디언즈 오브 갤럭시 1: 2.0<br>\n" +
+                "상어가족: 1.5<br>\n" +
+                "<p>누적 대여료: <em>3.5</em>" +
+                "<p>\n<p>적립 포인트: <em>2</em><p>"));
+    }
+
+    @Test
+    public void htmlStatement_2() {
+        //given
+        Movie movie1 = new Movie("가디언즈 오브 갤럭시 1", Movie.REGULAR);
+        Movie movie4 = new Movie("상어가족", Movie.CHILDRENS);
+
+        Customer customer = new Customer("홍길동");
+        customer.addRental(new Rental(movie1, 3));
+        customer.addRental(new Rental(movie4, 4));
+        //when
+        String htmlStatement = customer.htmlStatement();
+
+        //then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement, is("<h1>홍길동 고객님의 대여 기록</h1><p>\n" +
+                "가디언즈 오브 갤럭시 1: 3.5<br>\n" +
+                "상어가족: 3.0<br>\n" +
+                "<p>누적 대여료: <em>6.5</em><p>\n" +
+                "<p>적립 포인트: <em>2</em><p>"));
+    }
+
+    @Test
+    public void htmlStatement_3() {
+        //given
+        Movie movie1 = new Movie("가디언즈 오브 갤럭시 1", Movie.REGULAR);
+        Movie movie2 = new Movie("가디언즈 오브 갤럭시 2", Movie.REGULAR);
+        Movie movie3 = new Movie("서치", Movie.NEW_RELEASE);
+        Movie movie4 = new Movie("상어가족", Movie.CHILDRENS);
+
+        Customer customer = new Customer("홍길동");
+        customer.addRental(new Rental(movie1, 2));
+        customer.addRental(new Rental(movie2, 5));
+        customer.addRental(new Rental(movie3, 1));
+        customer.addRental(new Rental(movie4, 5));
+        //when
+        String htmlStatement = customer.htmlStatement();
+
+        //then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement, is("<h1>홍길동 고객님의 대여 기록</h1><p>\n" +
+                "가디언즈 오브 갤럭시 1: 2.0<br>\n" +
+                "가디언즈 오브 갤럭시 2: 6.5<br>\n" +
+                "서치: 3.0<br>\n" +
+                "상어가족: 4.5<br>\n" +
+                "<p>누적 대여료: <em>16.0</em><p>\n" +
+                "<p>적립 포인트: <em>4</em><p>"));
+    }
 }
