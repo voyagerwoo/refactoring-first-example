baadbbde373b12d51a0f45d6c6062137c7955112
voyagerwoo <voyager.woo@gmail.com>
2018-11-19 00:52:38 +0900

    가격 계산 메서드를 Movie로 이동

--- src/main/java/vw/study/refactoring/Movie.java
+++ src/main/java/vw/study/refactoring/Movie.java
@@ -24,4 +24,24 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this._priceCode = priceCode;
     }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
